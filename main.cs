879a8e55baed6a91d5f756a9b85a78af18869ca3
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2023-01-17 21:43:02 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,41 @@
+// Created by: Ekaterina
+// Created on: Jan 2023
+//
+// This program calculates pi
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int numberOfLoops;
+        int counter = 0;
+        int denominator = -1;
+        double pii = 0;
+
+        Console.WriteLine(" ");
+        Console.WriteLine("How accurate do you want PI?");
+        Console.WriteLine(" ");
+        Console.WriteLine("(The bigger your number, the more accurate it will be)");
+        Console.WriteLine(" ");
+
+        numberOfLoops = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(" ");
+
+
+        while (counter < numberOfLoops)
+        {
+            int numerator = 4;
+            denominator = denominator + 2;
+            double multiply = Math.Pow(-1, counter);
+            double aLittleBit = (numerator / denominator) * multiply;
+            pii = pii + aLittleBit;
+            counter++;
+        }
+        
+        Console.Clear();
+        Console.WriteLine("Your PI is: " + pii);
+        Console.WriteLine("\nDone.");
+    }
+}
