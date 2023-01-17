cf26ea1907bf51ff71952c67ffd703bde4723b62
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2023-01-17 22:00:47 +0000

    whitespace

--- main.cs
+++ main.cs
@@ -33,7 +33,6 @@ class Program
             pii = pii + aLittleBit;
             counter++;
         }
-        
         Console.Clear();
         Console.WriteLine("Your PI is: " + pii);
         Console.WriteLine("\nDone.");
